6eb07dd9ac081b6c753986d90d4adc0a2d920ede
Amber <ambergoswami9@gmail.com>
2025-05-28 02:39:24 +0530

    added architecture diag

--- src/graph_builder.py
+++ src/graph_builder.py
@@ -1,6 +1,8 @@
 from state import State
 from langgraph.graph import StateGraph, END
 from nodes import GraphNodes
+import os
+from IPython.display import Image
 
 
 def run_langgraph_pipeline():
@@ -13,6 +15,15 @@ def run_langgraph_pipeline():
     graph.add_edge("filtering", END)
 
     app = graph.compile()
+    image_bytes = app.get_graph().draw_mermaid_png()
+    file_name = "langgraph_pipeline.png"
+    if not os.path.exists(file_name):
+        with open(file_name, "wb") as f:
+            f.write(image_bytes)
+        print(f"Saved image as {file_name}")
+    else:
+        print(f"{file_name} already exists. Skipping save.")
+
     final_state = app.invoke({})
     print("Pipeline completed. filtered ids:", final_state["filtered_ids"])
     
